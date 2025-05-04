b876dd28d51b05374d5224bf4e477451c0e9ce40
EugeneViktP <popoff.zhenya@yandex.ru>
2025-05-04 11:14:15 +0200

    corrected Response and Request

--- register-office/src/main/java/edu/javacourse/register/view/MarriageRequest.java
+++ register-office/src/main/java/edu/javacourse/register/view/MarriageRequest.java
@@ -1,8 +1,27 @@
 package edu.javacourse.register.view;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class MarriageRequest implements Serializable {
 
 
+    private String husbandSurname;
+    private String husbandGivenName;
+    private String husbandPatronymic;
+    private LocalDate husbandDateBirth;
+    private String husbandPassportSeria;
+    private String husbandPassportNumber;
+    private LocalDate husbandPassportIssueDate;
+
+    private String wifeSurname;
+    private String wifeGivenName;
+    private String wifePatronymic;
+    private LocalDate wifeDateBirth;
+    private String wifePassportSeria;
+    private String wifePassportNumber;
+    private LocalDate wifePassportIssueDate;
+
+    private String marriageCertificateNumber;
+    private LocalDate marriageCertificateDate;
 }
